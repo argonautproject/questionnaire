b0f42cad135f91a30e1d02a1d594fcf6d73fa4e5
Healthedata1 <ehaas@healthedatainc.com>
2019-03-22 17:54:38 -0700

    set up for autobuild

--- docs/Extension_multiple_and.java
+++
@@ -1,7 +0,0 @@
-package http://fhir.org/guides/argonaut-questionnaire/ImplementationGuide/argonaut.us.questionnaire-1.0.0;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class Extension_multiple_and {
-
-}
